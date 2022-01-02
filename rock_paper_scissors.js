35da422f6df6dde3dbf0027ab64546be054d0611
Josh Fang <jfang.96@gmail.com>
2022-01-01 16:28:11 -0800

    Implement getWinner function

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,5 +1,33 @@
+console.log(getWinner("rock", "rock"));
+console.log(getWinner("rock", "paper"));
+console.log(getWinner("rock", "scissors"));
+
+
+
 function computerPlay() {
     const options = ['rock', 'paper', 'scissors'];
     return options[Math.floor(Math.random()*3)];
 }
 
+function getWinner(playerSelection, computerSelection) {
+    console.log(playerSelection);
+    console.log(computerSelection);
+
+    playerSelection = playerSelection.toLowerCase();
+    
+    if (playerSelection == computerSelection) {
+        return "It's a tie!";
+    }
+
+    const winMap = new Map([
+        ['rock', 'scissors'],
+        ['paper', 'rock'],
+        ['scissors', 'paper']
+    ]);
+
+    if (winMap.get(playerSelection) == computerSelection) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
