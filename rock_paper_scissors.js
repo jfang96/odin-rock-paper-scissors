4503d5597db37bac398f851386d35f790136f694
Josh Fang <jfang.96@gmail.com>
2022-01-08 18:37:07 -0800

    Remove game() function and add rock, paper, scissors buttons

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,4 +1,4 @@
-console.log(game());
+// console.log(game());
 
 function computerPlay() {
     const options = ['rock', 'paper', 'scissors'];
@@ -27,31 +27,31 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
-function game() {
-    playerWinCount = 0;
-    computerWinCount = 0;
-    for (let i = 0; i < 5; i++) {
-        playerSelection = window.prompt('Rock, paper, or scissors?');
-        winInt = 0;
-        // playerSelection = 'rock';
-        [win, message] = playRound(playerSelection, computerPlay());
+// function game() {
+//     playerWinCount = 0;
+//     computerWinCount = 0;
+//     for (let i = 0; i < 5; i++) {
+//         playerSelection = window.prompt('Rock, paper, or scissors?');
+//         winInt = 0;
+//         // playerSelection = 'rock';
+//         [win, message] = playRound(playerSelection, computerPlay());
         
-        console.log(message);    
-        switch (win) {
-            case 0:
-                break;
-            default:
-                win > 0 ? playerWinCount++ : computerWinCount++;
-        } 
-    }
+//         console.log(message);    
+//         switch (win) {
+//             case 0:
+//                 break;
+//             default:
+//                 win > 0 ? playerWinCount++ : computerWinCount++;
+//         } 
+//     }
 
 
-    if (playerWinCount == computerWinCount) {
-        return `It\'s a tie! ${playerWinCount}-${computerWinCount}`;
-    } else {
-        return playerWinCount > computerWinCount ? `You win ${playerWinCount}-${computerWinCount}!` : `Computer wins ${computerWinCount}-${playerWinCount}!`;
-    }
-}
+//     if (playerWinCount == computerWinCount) {
+//         return `It\'s a tie! ${playerWinCount}-${computerWinCount}`;
+//     } else {
+//         return playerWinCount > computerWinCount ? `You win ${playerWinCount}-${computerWinCount}!` : `Computer wins ${computerWinCount}-${playerWinCount}!`;
+//     }
+// }
 
 function capitalize(myStr) {
     return `${myStr[0].toUpperCase()}${myStr.substring(1).toLowerCase()}`;
